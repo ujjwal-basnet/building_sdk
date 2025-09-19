132ed2b58dbd77d3f0ac1bf50d22f6c202dc9c27
ujjwal <ujjwalvictus15@gmail.com>
2025-09-19 23:38:37 +0545

    chore(package): added   SWCConfig and SWCClient

--- src/swcpy/__init__.py
+++ src/swcpy/__init__.py
@@ -1,1 +1,4 @@
 ## since our sdk will be a python package , each directory that contains code will contain __init__.py  file 
+
+from .swc_config import SWCConfig 
+from .swc_client import SWCClient
